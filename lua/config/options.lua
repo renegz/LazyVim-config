b645b0487b95cab339ce3ec2cdec6604cdbacafe
renegz <00077321@uca.edu.sv>
2025-01-09 21:33:27 -0600

    :tada: LazyVim def settings

---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+vim.g.autoformat = false --Prevent autoformat on saving a file
+
+local opt = vim.opt
+
+opt.shiftwidth = 4  --Set indent on 4 spaces
