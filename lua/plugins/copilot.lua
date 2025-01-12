e919f66a926301bc9d22fa454db146731c0595dd
renegz <00077321@uca.edu.sv>
2025-01-11 23:56:04 -0600

    :sparkles: Copilot Plugin

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+opts = {
+  suggestion = {
+    enabled = not vim.g.ai_cmp,
+    auto_trigger = true,
+    keymap = {
+      accept = false, -- handled by nvim-cmp / blink.cmp
+      next = "<M-]>",
+      prev = "<M-[>",
+    },
+  },
+  panel = { enabled = false },
+  filetypes = {
+    markdown = true,
+    help = true,
+  },
+}
