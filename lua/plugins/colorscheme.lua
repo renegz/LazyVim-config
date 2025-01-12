df4af622b3a64799d048342db1593ce343731468
renegz <00077321@uca.edu.sv>
2025-01-12 00:37:36 -0600

    :lipstick: KanagawaWave colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- add gruvbox
+    {"rebelot/kanagawa.nvim"},
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-wave"
+    },
+  }
+}
+
